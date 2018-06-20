5cc651c92e664b4abb7e79e785ef814eb385bff7
unknown <ppastorino@gmail.com>
2018-06-20 11:24:02 -0300

    heroku

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const forceSSL = function() {
   // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/cotizacion-dolar/' + 'index.html'));
   });
 
 // Start the app by listening on the default
